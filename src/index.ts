5aeeaa9519e05527fc6b376795dd066f1a1b781e
p1yu5h0 <piyush200205@gmail.com>
2024-02-27 10:29:50 +0530

    request-type

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,15 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		console.log(request.body);
+		console.log(request.headers);
+		console.log(request.method);
+		var uri = request.url.replace(/^https:\/\/.*?\//gi, "/");
+		console.log(uri);
+		
+		return Response.json({
+			message: `you send a ${request.method} request`,
+			message2: `${uri}`
+		});
 	},
 };
